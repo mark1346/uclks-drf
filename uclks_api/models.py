7d273e0bfda5c5440e580f4dbc9dc896874672d0
Mark Han <markhan@Markui-MacBookAir.local>
2023-04-20 16:24:07 +0900

    uclks_api serializers.py and views.py coded

--- uclks_api/models.py
+++ uclks_api/models.py
@@ -7,34 +7,6 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-### profiles
-class Profiles(models.Model):
-    user = models.OneToOneField('Users', models.CASCADE)
-    name = models.CharField(max_length=255)
-    birthday = models.DateField(blank=True, null=True)
-    gender = models.IntegerField()
-    degree = models.IntegerField()
-    department = models.CharField(max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'profiles'
-
-### users
-class Users(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    email = models.CharField(unique=True, max_length=255)
-    email_verified_at = models.DateTimeField(blank=True, null=True)
-    password = models.CharField(max_length=255)
-    remember_token = models.CharField(max_length=100, blank=True, null=True)
-    role = models.PositiveIntegerField()
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'users'
-
 ### department_module
 class DepartmentModule(models.Model):
     department = models.ForeignKey('Departments', models.DO_NOTHING)
@@ -73,9 +45,10 @@ class Modules(models.Model):
 
 class AuthGroup(models.Model):
     name = models.CharField(unique=True, max_length=150)
+    id = models.AutoField(primary_key=True)
 
     class Meta:
-        id = models.AutoField(primary_key=True)
+
         db_table = 'auth_group'
 
 class AuthGroupPermissions(models.Model):
